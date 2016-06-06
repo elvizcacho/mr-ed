1fd3a7ad18f33a918e59fed09c16536639f6e024
Sebastian Vizcaino <elvizcacho@gmail.com>
2016-06-05 22:04:25 -0500

    mr-ed init done

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var Subscribers = require('./subscribers');
 var Events = require('./events');
 var fs = require('fs');
-var config = JSON.parse(fs.readFileSync('./config.json', 'utf-8'));
+var config = JSON.parse(fs.readFileSync(__dirname + '/config.json', 'utf-8'));
 
 function Ed() {}
 
-Ed.prototype.connection = function(data) {
+Ed.prototype.connect = function(data) {
 	if (!data.token) throw new Error('Not token provided');
 	if (!data.env) throw new Error('Not env provided');
-	if (data.env !== 'dev' || data.env !== 'live' || data.env !== 'production' || data.env !== 'local' || data.env !== 'development') throw new Error('Env value wrong. Try dev live or local');
+	if (!(data.env === 'dev' || data.env === 'live' || data.env === 'production' || data.env === 'local' || data.env === 'development')) throw new Error('Env value wrong. Try dev live or local');
 	this.token = data.token;
 	this.env = data.env;
-	this.url = config[this.env];
+	this.url = config.url[this.env];
 };
 
 Ed.prototype.Subscribers = Subscribers;
