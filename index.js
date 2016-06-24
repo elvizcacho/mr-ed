e2de1d26483c9c5ec287b605776848140bd8b52f
Sebastian Vizcaino <elvizcacho@gmail.com>
2016-06-24 16:50:57 -0500

    add extra data to events

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function Ed() {}
 
 Ed.prototype.router = function(app) {
 	app.post('/events/:event', function(req, res, next) {
-		emitter.emit(req.params.event, req.body.new, req.body.old);
+		emitter.emit(req.params.event, req.body.new, req.body.old, req.body.data);
 		res.send({
 			msg: 'ok'
 		});
